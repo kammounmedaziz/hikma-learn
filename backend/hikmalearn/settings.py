5130061a73086c29b3521fa70839ff8936243615
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2025-07-14 10:49:30 +0100

    Define user entity

--- backend/hikmalearn/settings.py
+++ backend/hikmalearn/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = 'accounts.User'
