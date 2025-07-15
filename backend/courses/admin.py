9992ab5bb0a5b15f6b50355d22d3043d6a1f4f51
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2025-07-15 11:29:06 +0100

    Add simple course admin management

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Course, Chapter, Content, CourseFollow, Quiz, ContentSeen
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'teacher', 'creation_date', 'updated_date')
+    search_fields = ('title', 'teacher__username')
+    list_filter = ('teacher',)
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order')
+    search_fields = ('title', 'course__title')
+    list_filter = ('course',)
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'chapter', 'content_kind', 'order')
+    search_fields = ('title', 'chapter__title')
+    list_filter = ('content_kind', 'chapter')
+
+@admin.register(CourseFollow)
+class CourseFollowAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'follow_date')
+    search_fields = ('student__username', 'course__title')
+    list_filter = ('course',)
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'creation_date')
+    search_fields = ('title', 'content__title')
+
+@admin.register(ContentSeen)
+class ContentSeenAdmin(admin.ModelAdmin):
+    list_display = ('student', 'content', 'seen_date')
+    search_fields = ('student__username', 'content__title')
+    list_filter = ('content',)
